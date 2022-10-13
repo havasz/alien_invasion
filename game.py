b350d365e701bd552335de68c093a58b27673f00
havasz <115722024+havasz@users.noreply.github.com>
2022-10-13 11:41:57 -0400

    231

--- game.py
+++ game.py
@@ -11,6 +11,9 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
 
+        # background color
+        self.bg_color = (230, 230, 230)
+
     def run_game(self):
         """start main loop"""
         while True:
@@ -19,9 +22,22 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
+            # redraw screen during each pass through loop
+            self.screen.fill(self.bg_color)
+
                     # make most recently drawn screen visable
                     pygame.display.flip()
 if __name__ == '__main__':
     # make a game instance and run game
     ai = AlienInvasion()
     ai.run_game()
+
+class Settings:
+    """class to store all settings"""
+
+    def __int__(self):
+        """initialize game settings"""
+        # screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
