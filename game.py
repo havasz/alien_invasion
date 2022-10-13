b94c532d19b8bafb257215f44d3c3c0166c894be
havasz <115722024+havasz@users.noreply.github.com>
2022-10-13 11:33:56 -0400

    pg 229

--- game.py
+++ game.py
@@ -1,1 +1,27 @@
+import sys
 import pygame
+
+class AlienInvasion:
+    """overall class to manage assets and behavior"""
+
+    def __int__(self):
+        """initialize the game and create game resources"""
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+        """start main loop"""
+        while True:
+            # watch for keyboard and mouse events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+                    # make most recently drawn screen visable
+                    pygame.display.flip()
+if __name__ == '__main__':
+    # make a game instance and run game
+    ai = AlienInvasion()
+    ai.run_game()
